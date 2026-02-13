6abeb117433c8e4a9fee3d110e1f785fd01adf39
Juan Carlos <jcsanchezs@gmail.com>
2026-02-13 18:30:54 +0100

    Tercer push

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response, redirect
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hola a toda la clase</h1>"
+    return render_template("index.html")
+@app.route("/from_login")
+def login():
+    return render_template("login.html")
+
+@app.route('/sign_in', methods=['POST'])  # Define la ruta para el método POST en /sign_in
+def sign_in():
+    # Obtener los datos del formulario (login y contraseña)
+    login = request.form['login']  # Obtiene el valor del campo 'login' (correo o usuario)
+    passwd = request.form['passwd']  # Obtiene el valor del campo 'passwd' (contraseña)
+
+    # Imprimir el usuario y la contraseña en la consola (para propósitos de depuración)
+    print(f'Usuario: {login}')  # Muestra el login (usuario o correo)
+    print(f'Contraseña: {passwd}')  # Muestra la contraseña
+
+    # Crear una respuesta de redirección a la página '/login_ok'
+    response = make_response(redirect('/login_ok'))  # Redirige a la página '/login_ok'
+
+    # Establecer una cookie con el token JWT (ejemplo en este caso)
+    response.set_cookie('token', 'esto es un token de ejemplo')  # Se establece una cookie llamada 'token'
+
+    # Establecer una cookie con el nombre de usuario (ejemplo en este caso)
+    response.set_cookie('userlogin', 'el usuario')  # Se establece una cookie llamada 'userlogin'
+
+    # Devolver la respuesta con las cookies configuradas
+    return response  # Retorna la respuesta con las cookies y la redirección
 @app.route("/saludo/<nombre>")
 def saludo(nombre):
     return f"HOLA! {nombre}"
